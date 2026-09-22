4ff923748ab151ee723bccb7018efed8cd7d525b
chiscookeke11 <chiscookeke11@gmail.com>
2026-09-22 08:08:48 +0100

    added the frontend url to the CORS allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://192.168.0.176:3000",
+        "https://frontend-ten-virid-91.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
